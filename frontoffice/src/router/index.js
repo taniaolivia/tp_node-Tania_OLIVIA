de8fa0639b02106c04f918f4120b2d609567e7ec
taniaolivia <taniaolivia@macbook-pro-de-tania.home>
2022-11-10 19:59:49 +0100

    add all routes

---
+++ frontoffice/src/router/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+    {
+        path: '/',
+        name: 'HomePage',
+        component: () =>
+            import ('../views/HomePage.vue')
+    },
+    {
+        path: '/login',
+        name: 'LoginPage',
+        component: () =>
+            import ('../views/LoginPage.vue')
+    },
+    {
+        path: '/register',
+        name: 'RegisterPage',
+        component: () =>
+            import ('../views/RegisterPage.vue')
+    },
+    {
+        path: '/posts',
+        name: 'ListPostsPage',
+        component: () =>
+            import ('../views/ListPostsPage.vue')
+    },
+    {
+        path: '/addPost',
+        name: 'CreatePostPage',
+        component: () =>
+            import ('../views/CreatePostPage.vue')
+    }
+]
+
+const router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+})
+
+export default router
