78c2a640f7cf3cecd74ef820fc4c24f7d93f6aa3
taniaolivia <taniaolivia@macbook-pro-de-tania.home>
2022-11-10 19:59:21 +0100

    add store

---
+++ frontoffice/src/store/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import VuexPersistence from 'vuex-persist'
+
+Vue.use(Vuex)
+
+const vuexLocal = new VuexPersistence({
+    storage: window.localStorage
+})
+export default new Vuex.Store({
+    plugins: [vuexLocal.plugin],
+    state: {
+        token: ""
+    },
+    mutations: {
+        setToken(state, token) {
+            state.token = token
+        },
+        setUser(state, user) {
+            state.user = user
+        }
+    },
+    actions: {},
+    modules: {}
+})
